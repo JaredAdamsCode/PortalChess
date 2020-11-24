d19efd008d0e9ff4058cf2a12e0746fe7f40fe8d
Kongleochi <54540110+Kongleochi@users.noreply.github.com>
2020-11-24 14:21:47 -0700

    Import IllegalMoveException into MatchController.

--- demo/src/main/java/com/example/demo/MatchController.java
+++ demo/src/main/java/com/example/demo/MatchController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.chessboard.ChessBoard;
+import com.example.demo.chessboard.IllegalMoveException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.json.JSONArray;
 import org.json.JSONException;
